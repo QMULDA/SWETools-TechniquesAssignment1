159e428849e282dcd6ce740d6d79b3d8298ad8c6
Victoria S <ec24599@qmul.ac.uk>
2025-02-19 20:26:31 +0000

    make ExceptionQuine public

--- src/main/java/org/vsapry/ExceptionQuine.java
+++ src/main/java/org/vsapry/ExceptionQuine.java
@@ -4,7 +4,7 @@ public class ExceptionQuine extends Exception {
 
 	private static final long serialVersionUID = 1L;
 
-	ExceptionQuine(String str) {
+	public ExceptionQuine(String str) {
 
 		super(str);
 
