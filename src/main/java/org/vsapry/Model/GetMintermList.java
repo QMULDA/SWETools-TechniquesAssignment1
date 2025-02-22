27287d3b14aba0024ec389ce8f6766fce9e1c6a2
Victoria S <ec24599@qmul.ac.uk>
2025-02-22 20:47:07 +0000

    remove unused warning and unused minList variable

--- src/main/java/org/vsapry/Model/GetMintermList.java
+++ src/main/java/org/vsapry/Model/GetMintermList.java
@@ -1,19 +1,20 @@
-package org.vsapry;
+package org.vsapry.Model;
 
 import java.util.Set;
 import java.util.TreeSet;
 
 public class GetMintermList {
+
 	static Set<String> set=new TreeSet<String>();
-	
-	@SuppressWarnings("unused")
-	private static 	String[] minList=new String[256];
+
 	
 	public void setMinList(String x){
 	
 		set.add(x);
 		
 	}
+
+//TODO REROUTE ALL THE GUI CALLS TO MINTERM LIST TO MINTERMCONTROLLLER
 
 
 public static Set<String> getMin(){
