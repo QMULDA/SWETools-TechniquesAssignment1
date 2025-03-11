6de8b909dcad5ac2de40b2fd97b11c1a5fabed15
Victoria S <ec24599@qmul.ac.uk>
2025-03-11 00:50:11 +0000

    test Quine and MinTerm

---
+++ src/test/java/MinTermTest.java
@@ -0,0 +1,75 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.vsapry.Model.MinTerm;
+
+public class MinTermTest {
+
+    private MinTerm minTermA;
+    private MinTerm minTermB;
+
+    @BeforeEach
+    void setUp() {
+        minTermA = new MinTerm("0");
+        minTermB = new MinTerm("1");
+    }
+
+    @Test
+    void testToString() {
+        assertEquals("0", minTermA.toString());
+        assertEquals("1", minTermB.toString());
+    }
+
+    @Test
+    void testIsSameTrue() {
+        MinTerm anotherA = new MinTerm("0");
+        assertTrue(minTermA.isSame(anotherA));
+    }
+
+    @Test
+    void testIsSameFalse() {
+        assertFalse(minTermA.isSame(minTermB));
+    }
+
+    @Test
+    void testIsSameThrowsExceptionWhenMinTermInvalidLength() {
+        MinTerm twoChars = new MinTerm("00");
+        assertThrows(IllegalArgumentException.class, () -> {minTermA.isSame(twoChars);});
+    }
+
+    @Test
+    void testIsSameThrowsExceptionWhenLengthMinTermsMismatched() {
+        MinTerm twoChars = new MinTerm("00");
+        assertThrows(IllegalArgumentException.class, () -> {minTermA.isSame(twoChars);});
+    }
+
+    @Test
+    void testNumberOfDifferencesBetweenMinTermsIs1() {
+        int numberOfDifferences = minTermA.numberOfDifferencesBetweenMinTerms(minTermB);
+        assertEquals(1, numberOfDifferences);
+    }
+
+    @Test
+    void testNumberOfDifferencesBetweenMinTermsIs3(){
+        MinTerm a = new MinTerm("0000");
+        MinTerm b = new MinTerm("0111");
+
+        assertEquals(3, a.numberOfDifferencesBetweenMinTerms(b));
+    }
+
+    @Test
+    void testNumberOfDifferencesBetweenMinTermsThrowsException(){
+        MinTerm twoChars = new MinTerm("00");
+        assertThrows(IllegalArgumentException.class, () -> {minTermA.numberOfDifferencesBetweenMinTerms(twoChars);});
+    }
+
+    @Test
+    void testCombine(){
+        MinTerm a = new MinTerm("0111");
+        MinTerm b = new MinTerm("1111");
+
+        assertEquals("_111", MinTerm.combine(a, b).toString());
+    }
+
+}
--- src/test/java/QuineTest.java
+++ src/test/java/QuineTest.java
@@ -1,34 +1,126 @@
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.vsapry.*;
+import org.vsapry.Model.MinTerm;
 import org.vsapry.Model.Quine;
+
+import javax.naming.LimitExceededException;
 
 public class QuineTest {
 
-    //given, when, then
-    //e.g. givenRadius_whenCalculateArea_thenReturnArea
+    private Quine quine;
 
-    @Test
-    public void addTermTest() throws ExceptionQuine {
-
-        String str = "0000";
-        Quine quine = new Quine();
-        quine.addTerm(str);
-        assertEquals(str,quine.terms[0].toString());
+    @BeforeEach
+    void setUp() {
+        quine = new Quine();
     }
 
     @Test
-    public void addTermTestThrowsException() throws Exception {
-        String str = "0000";
-        Quine quine = new Quine();
-        quine.count = 0xff;
+    public void testAddTerm() throws LimitExceededException {
+        quine.addTerm("0");
+        quine.addTerm("1");
 
-        ExceptionQuine q = assertThrows(ExceptionQuine.class, () -> {
-            quine.addTerm(str);
-        });
-        assertTrue(q.getMessage().contains("Quine::addTerm()"));
+        assertEquals(2, quine.numMinTermsInTerm);
 
+        assertTrue(quine.hasTerm(new MinTerm("0")));
+        assertTrue(quine.hasTerm(new MinTerm("1")));
+    }
+
+    @Test
+    public void testExceptionThrownIfAddTermExceedsMax() throws LimitExceededException {
+        for (int i = 0; i < 255; i++) {
+            quine.addTerm(String.valueOf(i));
+        }
+
+        assertThrows(LimitExceededException.class, () -> quine.addTerm("255"));
+    }
+
+    @Test
+    void testAddTermWithDuplicateTerms() throws LimitExceededException {
+        quine.addTerm("0");
+        quine.addTerm("0");
+
+        assertEquals(2, quine.numMinTermsInTerm);
+    }
+
+    //don't need to test addTerm for string input because input is validated in MinTermFactory
+
+    @Test
+    public void testToString() throws LimitExceededException {
+        quine.addTerm("0");
+        quine.addTerm("1");
+
+        String output = quine.toString();
+        assertNotNull(output);
+        assertFalse(output.isBlank());
+
+        assertTrue(output.contains("0"), "Expected '0' in the output string.");
+        assertTrue(output.contains("1"), "Expected '1' in the output string.");
+    }
+
+    @Test
+    void testHasTermWhenTermDoesNotExistReturnsFalse() throws LimitExceededException {
+        quine.addTerm("0");
+
+        assertFalse(quine.hasTerm(new MinTerm("1")));
     }
 
 
+
+    //test hasTerm throws exception - or not, since it's thrown from isSame
+
+    @Test
+    public void testSimplifyAndReduce() throws LimitExceededException {
+        quine.addTerm("0");
+        quine.addTerm("1");
+
+        quine.simplify();
+
+        assertTrue(quine.numMinTermsInTerm > 0, "There should be at least one prime implicant");
+        assertTrue(quine.numMinTermsInTerm <= 4, "Some minterms should be combined, so count should be <= 4.");
+    }
+
+    @Test
+    void testNoMergesInReduceFunctionWhenMinTermsDifferInMoreThan1Bit() throws LimitExceededException {
+        quine.addTerm("000");
+        quine.addTerm("111");
+
+        int reduced = quine.reduce();
+        assertEquals(0, reduced);
+        assertEquals(2, quine.numMinTermsInTerm);
+    }
+
+    @Test
+    void testReduceSingleMergeWhenMinTermsDifferBy1Bit() throws LimitExceededException {
+        quine.addTerm("000");
+        quine.addTerm("001");
+        int reduced = quine.reduce();
+        assertEquals(1, reduced);
+        assertTrue(quine.toString().contains("00_"));
+    }
+
+    @Test
+    void testReduce1OccurencesOfAMerge() throws LimitExceededException {
+        quine.addTerm("000");
+        quine.addTerm("001");
+        quine.addTerm("010");
+
+        int reduced = quine.reduce();
+        assertEquals(2, reduced);
+        assertTrue(quine.toString().contains("0_0"));
+        assertTrue(quine.toString().contains("00_"));
+    }
+
+    @Test
+    void testReduce2OccurencesOfMerges() throws LimitExceededException {
+        quine.addTerm("000");
+        quine.addTerm("001");
+        quine.addTerm("010");
+        quine.addTerm("101");
+
+        quine.simplify();
+        assertEquals(3, quine.numMinTermsInTerm);
+    }
 }
